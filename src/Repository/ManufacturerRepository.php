624e4b9c199595d93b63ff962eb5062e2aeeae4d
jakubkoval <Jakub@techpike.net>
2018-11-22 14:28:20 +0000

    Added lesson related code

---
+++ src/Repository/ManufacturerRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Manufacturer;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Manufacturer|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Manufacturer|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Manufacturer[]    findAll()
+ * @method Manufacturer[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ManufacturerRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Manufacturer::class);
+    }
+
+    // /**
+    //  * @return Manufacturer[] Returns an array of Manufacturer objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('m')
+            ->andWhere('m.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('m.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Manufacturer
+    {
+        return $this->createQueryBuilder('m')
+            ->andWhere('m.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
